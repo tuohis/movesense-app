a320c7855e70b84576db2c422f930d65de9109c1
Mikko Tuohimaa <mikko.tuohimaa@iki.fi>
2019-07-02 20:38:36 +0300

    Add the average acceleration endpoint and implement the business logic to produce the result
    Developer-configurable parameters via #defines:
    - accelerometer sampling frequency
    - result publishing interval

---
+++ avg-acceleration-app/AccelerometerSumService.cpp
@@ -0,0 +1,264 @@
+#include "AccelerometerSumService.h"
+#include "app-resources/resources.h"
+#include "common/core/debug.h"
+#include "meas_acc/resources.h"
+#include "whiteboard/builtinTypes/UnknownStructure.h"
+
+#include <float.h>
+#include <math.h>
+
+// CONFIGURABLE VALUES START
+// ----------------------------------------------------------------------
+
+// The desired value publish interval in milliseconds
+#define PUBLISH_INTERVAL_MS 1000
+
+// The rate with which the accelerometer is operating. Can be one of
+// 13, 26, 52, 104, 208, 416, 833, 1666
+#define ACC_SAMPLE_RATE 13
+
+// ----------------------------------------------------------------------
+// CONFIGURABLE VALUES END
+
+static_assert(
+    ACC_SAMPLE_RATE == 13 || ACC_SAMPLE_RATE == 26 || ACC_SAMPLE_RATE == 52 ||
+    ACC_SAMPLE_RATE == 104 || ACC_SAMPLE_RATE == 208 || ACC_SAMPLE_RATE == 416 ||
+    ACC_SAMPLE_RATE == 833 || ACC_SAMPLE_RATE == 1666,
+    "Selected accelerometer sample rate not supported"
+);
+
+// Some preprocessor magic for concatenating strings and numbers
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+#define ACCELEROMETER_RESOURCE_ENDPOINT "Meas/Acc/" STR(ACC_SAMPLE_RATE)
+
+const char* const AccelerometerSumService::LAUNCHABLE_NAME = "AccSum";
+static const whiteboard::ExecutionContextId sExecutionContextId =
+    WB_RES::LOCAL::EXERCISE_SUMVECTOR_MIKKO::EXECUTION_CONTEXT;
+
+static const whiteboard::LocalResourceId sProviderResources[] = {
+    WB_RES::LOCAL::EXERCISE_SUMVECTOR_MIKKO::LID,
+};
+
+AccelerometerSumService::AccelerometerSumService()
+    : ResourceClient(WBDEBUG_NAME(__FUNCTION__), sExecutionContextId),
+      ResourceProvider(WBDEBUG_NAME(__FUNCTION__), sExecutionContextId),
+      LaunchableModule(LAUNCHABLE_NAME, sExecutionContextId),
+      isRunning(false)
+{
+}
+
+AccelerometerSumService::~AccelerometerSumService()
+{
+}
+
+bool AccelerometerSumService::initModule()
+{
+    if (registerProviderResources(sProviderResources) != whiteboard::HTTP_CODE_OK)
+    {
+        return false;
+    }
+
+    mModuleState = WB_RES::ModuleStateValues::INITIALIZED;
+    return true;
+}
+
+void AccelerometerSumService::deinitModule()
+{
+    unregisterProviderResources(sProviderResources);
+    mModuleState = WB_RES::ModuleStateValues::UNINITIALIZED;
+}
+
+/** @see whiteboard::ILaunchableModule::startModule */
+bool AccelerometerSumService::startModule()
+{
+    mModuleState = WB_RES::ModuleStateValues::STARTED;
+
+    return true;
+}
+
+void AccelerometerSumService::onUnsubscribeResult(whiteboard::RequestId requestId,
+                                                     whiteboard::ResourceId resourceId,
+                                                     whiteboard::Result resultCode,
+                                                     const whiteboard::Value& rResultData)
+{
+    DEBUGLOG("AccelerometerSumService::onUnsubscribeResult() called.");
+}
+
+void AccelerometerSumService::onSubscribeResult(whiteboard::RequestId requestId,
+                                                   whiteboard::ResourceId resourceId,
+                                                   whiteboard::Result resultCode,
+                                                   const whiteboard::Value& rResultData)
+{
+    DEBUGLOG("AccelerometerSumService::onSubscribeResult() called. resourceId: %u, result: %d", resourceId.localResourceId, (uint32_t)resultCode);
+
+    whiteboard::Request relatedIncomingRequest;
+    bool relatedRequestFound = mOngoingRequests.get(requestId, relatedIncomingRequest);
+
+    if (relatedRequestFound)
+    {
+        returnResult(relatedIncomingRequest, wb::HTTP_CODE_OK);
+    }
+}
+
+whiteboard::Result AccelerometerSumService::startRunning(whiteboard::RequestId& remoteRequestId)
+{
+    if (isRunning)
+    {
+        return whiteboard::HTTP_CODE_OK;
+    }
+
+    DEBUGLOG("AccelerometerSumService::startRunning()");
+
+    // Reset max acceleration members
+    this->accValue = whiteboard::FloatVector3D();
+    this->mSamplesIncluded = 0;
+
+    wb::Result result = getResource(ACCELEROMETER_RESOURCE_ENDPOINT, mMeasAccResourceId);
+    if (!wb::RETURN_OKC(result))
+    {
+        return result;
+    }
+
+    result = asyncSubscribe(mMeasAccResourceId, AsyncRequestOptions(&remoteRequestId, 0, true));
+
+    if (!wb::RETURN_OKC(result))
+    {
+        DEBUGLOG("asyncSubscribe threw error: %u", result);
+        return whiteboard::HTTP_CODE_BAD_REQUEST;
+    }
+    isRunning = true;
+
+    return whiteboard::HTTP_CODE_OK;
+}
+
+whiteboard::Result AccelerometerSumService::stopRunning()
+{
+    if (!isRunning)
+    {
+        return whiteboard::HTTP_CODE_OK;
+    }
+
+    if (isResourceSubscribed(WB_RES::LOCAL::EXERCISE_SUMVECTOR_MIKKO::ID) == wb::HTTP_CODE_OK)
+    {
+        DEBUGLOG("AccelerometerSumService::stopRunning() skipping. Subscribers still exist.");
+        return whiteboard::HTTP_CODE_OK;
+    }
+
+    DEBUGLOG("AccelerometerSumService::stopRunning()");
+
+    // Unsubscribe the LinearAcceleration resource, when unsubscribe is done, we get callback
+    wb::Result result = asyncUnsubscribe(mMeasAccResourceId, NULL);
+    if (!wb::RETURN_OKC(result))
+    {
+        DEBUGLOG("asyncUnsubscribe threw error: %u", result);
+    }
+    isRunning = false;
+    releaseResource(mMeasAccResourceId);
+
+    return whiteboard::HTTP_CODE_OK;
+}
+
+// This callback is called when the resource we have subscribed notifies us
+void AccelerometerSumService::onNotify(whiteboard::ResourceId resourceId, const whiteboard::Value& value,
+                                          const whiteboard::ParameterList& parameters)
+{
+    DEBUGLOG("onNotify() called.");
+
+    // Confirm that it is the correct resource
+    if (resourceId.localResourceId == WB_RES::LOCAL::MEAS_ACC_SAMPLERATE::LID)
+    {
+        const WB_RES::AccData& linearAccelerationValue = value.convertTo<const WB_RES::AccData&>();
+
+        const whiteboard::Array<whiteboard::FloatVector3D>& arrayData = linearAccelerationValue.arrayAcc;
+
+        for (const auto& sample : arrayData)
+        {
+            this->accValue += sample;
+        }
+
+        this->mSamplesIncluded += arrayData.size();
+
+
+        uint32_t relativeTime = linearAccelerationValue.timestamp;
+
+        // Publish a new value if it's time. Take variable overflow into account
+        if (relativeTime > this->previousPublishTimestamp + PUBLISH_INTERVAL_MS || relativeTime < this->previousPublishTimestamp)
+        {
+            // Reset counter and notify our subscribers
+            WB_RES::SampleDataValue sampleDataValue;
+            sampleDataValue.relativeTime = relativeTime;
+            sampleDataValue.xValue = this->accValue.x / this->mSamplesIncluded;
+            sampleDataValue.yValue = this->accValue.y / this->mSamplesIncluded;
+            sampleDataValue.zValue = this->accValue.z / this->mSamplesIncluded;
+
+            // Reset members
+            this->mSamplesIncluded = 0;
+            this->accValue = whiteboard::FloatVector3D();
+            this->previousPublishTimestamp = relativeTime;
+
+            // and update our WB resource. This causes notification to be fired to our subscribers
+            updateResource(WB_RES::LOCAL::EXERCISE_SUMVECTOR_MIKKO(),
+                           ResponseOptions::Empty, sampleDataValue);
+        }
+    }
+}
+
+void AccelerometerSumService::onSubscribe(const whiteboard::Request& request,
+                                             const whiteboard::ParameterList& parameters)
+{
+    switch (request.getResourceConstId())
+    {
+    case WB_RES::LOCAL::EXERCISE_SUMVECTOR_MIKKO::ID:
+    {
+        // Someone subscribed to our service. Start collecting data and notifying when our service changes state (every 10 seconds)
+        whiteboard::RequestId remoteRequestId;
+        whiteboard::Result result = startRunning(remoteRequestId);
+
+        if (isRunning)
+        {
+            return returnResult(request, whiteboard::HTTP_CODE_OK);
+        }
+
+        if (!whiteboard::RETURN_OK(result))
+        {
+            return returnResult(request, result);
+        }
+        bool queueResult = mOngoingRequests.put(remoteRequestId, request);
+        (void)queueResult;
+        WB_ASSERT(queueResult);
+        break;
+    }
+    default:
+        ASSERT(0); // Should not happen
+    }
+}
+
+void AccelerometerSumService::onUnsubscribe(const whiteboard::Request& request,
+                                               const whiteboard::ParameterList& parameters)
+{
+    switch (request.getResourceConstId())
+    {
+    case WB_RES::LOCAL::EXERCISE_SUMVECTOR_MIKKO::ID:
+        stopRunning();
+        returnResult(request, wb::HTTP_CODE_OK);
+        break;
+
+    default:
+        returnResult(request, wb::HTTP_CODE_BAD_REQUEST);
+        ASSERT(0); // Should not happen
+    }
+}
+
+void AccelerometerSumService::onRemoteWhiteboardDisconnected(whiteboard::WhiteboardId whiteboardId)
+{
+    DEBUGLOG("AccelerometerSumService::onRemoteWhiteboardDisconnected()");
+    stopRunning();
+}
+
+void AccelerometerSumService::onClientUnavailable(whiteboard::ClientId clientId)
+{
+    DEBUGLOG("AccelerometerSumService::onClientUnavailable()");
+    stopRunning();
+}
---
+++ avg-acceleration-app/AccelerometerSumService.h
@@ -0,0 +1,130 @@
+#pragma once
+
+#include <whiteboard/LaunchableModule.h>
+#include <whiteboard/ResourceClient.h>
+#include <whiteboard/ResourceProvider.h>
+#include <whiteboard/containers/RequestMap.h>
+
+#include "wb-resources/resources.h"
+
+class AccelerometerSumService FINAL : private whiteboard::ResourceClient,
+                                      private whiteboard::ResourceProvider,
+                                      public whiteboard::LaunchableModule
+
+{
+public:
+    /** Name of this class. Used in StartupProvider list. */
+    static const char* const LAUNCHABLE_NAME;
+    AccelerometerSumService();
+    ~AccelerometerSumService();
+
+private:
+    /** @see whiteboard::ILaunchableModule::initModule */
+    virtual bool initModule() OVERRIDE;
+
+    /** @see whiteboard::ILaunchableModule::deinitModule */
+    virtual void deinitModule() OVERRIDE;
+
+    /** @see whiteboard::ILaunchableModule::startModule */
+    virtual bool startModule() OVERRIDE;
+
+    /** @see whiteboard::ILaunchableModule::stopModule */
+    virtual void stopModule() OVERRIDE { mModuleState = WB_RES::ModuleStateValues::STOPPED; }
+
+    /**
+    *	Subscribe notification callback.
+    *
+    *	@param request Request information
+    *	@param parameters List of parameters
+    */
+    virtual void onSubscribe(const whiteboard::Request& request,
+                             const whiteboard::ParameterList& parameters) OVERRIDE;
+
+    /**
+    *	Unsubscribe notification callback.
+    *
+    *	@param request Request information
+    *	@param parameters List of parameters
+    */
+    virtual void onUnsubscribe(const whiteboard::Request& request,
+                               const whiteboard::ParameterList& parameters) OVERRIDE;
+
+    /**
+    *  Whiteboard disconnect notification handler.
+    *
+    *  This can be used for example to cleanup possible subscription related information of clients from
+    *  the remote whiteboard.
+    *
+    *  @param whiteboardId ID of the whiteboard that has been disconnected.
+    *
+    *  @see whiteboard::ResourceProvider::onSubscribe
+    *  @see whiteboard::ResourceProvider::onUnsubscribe
+    */
+    virtual void onRemoteWhiteboardDisconnected(whiteboard::WhiteboardId whiteboardId) OVERRIDE;
+
+    /**
+    * Local client 'disconnect' notification handler.
+    *
+    *  This can be used for example to cleanup possible subscription related information of the client.
+    *
+    *  @see whiteboard::ResourceProvider::onSubscribe
+    *  @see whiteboard::ResourceProvider::onUnsubscribe
+    */
+    virtual void onClientUnavailable(whiteboard::ClientId clientId) OVERRIDE;
+
+    /**
+    *	Callback for resource notifications.
+    *   Note that this function will not be called for notifications that are
+    *   of types WB_RESOURCE_NOTIFICATION_TYPE_INSERT or WB_RESOURCE_NOTIFICATION_TYPE_DELETE,
+    *   just for notifications that are of type WB_RESOURCE_NOTIFICATION_TYPE_UPDATE.
+    *
+    *	@param resourceId Resource id associated with the update
+    *	@param rValue Current value of the resource
+    */
+    virtual void onNotify(whiteboard::ResourceId resourceId, const whiteboard::Value& value,
+                          const whiteboard::ParameterList& parameters);
+
+    /**
+    *	Callback for asynchronous UNSUBSCRIBE requests
+    *
+    *	@param requestId ID of the request
+    *	@param resourceId Successful request contains ID of the resource
+    *	@param resultCode Result code of the request
+    *	@param rResultData Successful result contains the request result
+    */
+    virtual void onUnsubscribeResult(whiteboard::RequestId requestId,
+                                     whiteboard::ResourceId resourceId,
+                                     whiteboard::Result resultCode,
+                                     const whiteboard::Value& rResultData);
+    /**
+    *	Callback for asynchronous SUBSCRIBE requests
+    *
+    *	@param requestId ID of the request
+    *	@param resourceId Successful request contains ID of the resource
+    *	@param resultCode Result code of the request
+    *	@param rResultData Successful result contains the request result
+    */
+    virtual void onSubscribeResult(whiteboard::RequestId requestId,
+                                   whiteboard::ResourceId resourceId,
+                                   whiteboard::Result resultCode,
+                                   const whiteboard::Value& rResultData);
+
+private:
+    whiteboard::Result startRunning(whiteboard::RequestId& remoteRequestId);
+    whiteboard::Result stopRunning();
+
+    whiteboard::ResourceId	mMeasAccResourceId;
+
+    whiteboard::RequestMap<2, void> mOngoingRequests; // For storing relations of incoming & outgoing requests
+
+    bool isRunning;
+
+    // The number of samples included in the accumulator value
+    size_t mSamplesIncluded;
+
+    // The (relative) timestamp of the previous published value in milliseconds
+    uint32_t previousPublishTimestamp;
+
+    // The accumulated sum vector of acceleration
+    whiteboard::FloatVector3D accValue;
+};
